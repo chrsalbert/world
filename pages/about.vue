481df46f06cadb73259a5525aa1913df42ff2a69
Christian <chrs.albert@gmail.com>
2020-04-21 17:06:59 +0200

    testint transition

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,20 @@
 <template>
-    <div>yolo
+    <div class="container2">
+        <h1>About me</h1>
+        <p>Lipsum dolor ojedosjoda jda kjasd lkds andsa lnasd lnasdlnsd alndsaladsn</p>
         <nuxt-link to="/">Back</nuxt-link>
     </div>
 </template>
 <script>
 export default {
-  transition: 'bounce'
+  transition: 'slide-left'
 }
 </script>
+<style>
+  .container2 {
+    text-align: center;
+    padding-top: 4rem;
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
