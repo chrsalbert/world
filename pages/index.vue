03c42cbc6b8af46ba1b8014f8e2e4e1a3bb3d686
Christian <chrs.albert@gmail.com>
2020-05-04 19:44:45 +0200

    refactores index

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,27 @@
 <template>
     <div id="index" class="u-scrollContainer">
         <div class="grid">
-            <div class="nav">
+            <div class="grid__nav">
                 <MainNavigation />
             </div>
-            <div class="video">
+            <div class="grid__video">
                 <div class="u-ghost" style="--delay: .16s;--duration: .44s">
-                    <video autoplay muted loop playsinline>
+                    <video class="video" autoplay muted loop playsinline>
                         <source src="/hero.mp4" type="video/mp4">
                     </video>
                 </div>
             </div>
-            <div class="text u-ghost" style="--delay: .08s;--duration: .52s">
-                <h1>1x Ost-Europa, Kaukasus, Asien & zurück</h1>
-                <FactsList :facts="facts" mode="horizontal" />
+            <div class="grid__main">
+                <div class="main u-ghost" style="--delay: .08s;--duration: .52s">
+                    <h1>1x Ost-Europa, Kaukasus, Asien & zurück</h1>
+                    <FactsList :facts="facts" mode="horizontal" />
+                </div>
             </div>
-            <div class="date">
-                <span class="u-ghost" style="--duration: .6s">MAI 2019 – MÄRZ 2020</span>
+            <div class="grid__date">
+                <span class="date u-ghost" style="--duration: .6s">Mai 2019 – März 2020</span>
             </div>
-            <div class="worldmap" v-html="worldmap"></div>
-            <div class="button">
+            <div class="grid__map" v-html="worldmap"></div>
+            <div class="grid__button">
                 <nuxt-link to="/journey/niederlande" class="a-button">Start</nuxt-link>
             </div>
         </div>
@@ -66,57 +68,72 @@
     }
 </script>
 <style scoped>
-.grid {
-    max-width: var(--body-width);
-    margin: 0 auto;
-    min-height: 100vh;
-    display: grid;
-    grid-template-columns: repeat(12, 1fr);
-    grid-template-rows: 80px minmax(10rem, 2fr) min-content minmax(0, 1fr);
-    gap: 0px 16px;
-}
+    h1 {
+        margin: 0 0 var(--space-lg)
+    }
 
-.nav { 
-    display: flex;
-    align-items: center;
-    grid-area: 1 / 1 / 2 / 13; 
-}
+    .grid {
+        max-width: var(--body-width);
+        margin: 0 auto;
+        min-height: 100vh;
+        display: grid;
+        grid-template-columns: repeat(12, 1fr);
+        grid-template-rows: 80px minmax(10rem, 2fr) min-content minmax(0, 1fr);
+        gap: 0px 16px;
+    }
 
-.video { 
-    grid-area: 2 / 5 / 5 / 13;
-    padding: 0 var(--space-md);
-}
+    .grid__nav { 
+        display: flex;
+        align-items: center;
+        grid-area: 1 / 1 / 2 / 13; 
+        padding-left: var(--space-md)
+    }
 
-.text { 
-    position: relative;
-    z-index: 1;
-    grid-area: 3 / 2 / 4 / 7;
-    background: var(--body-bgColor);
-    text-align: center;
-    padding: var(--space-md);
-}
+    .grid__video { 
+        grid-area: 2 / 5 / 5 / 13;
+        padding: 0 var(--space-md);
+    }
 
-.button { 
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    position: relative;
-    z-index: 2;
-    grid-area: 1 / 11 / 6 / 13;
-}
+    .video {
+        box-shadow: var(--shadow-lg)
+    }
 
-h1 {
-    margin: 0 0 var(--space-lg)
-}
+    .grid__main { 
+        position: relative;
+        z-index: 1;
+        grid-area: 3 / 2 / 4 / 7;
+    }
 
-.date { 
-    grid-area: 2 / 1 / 5 / 2; 
-    writing-mode: vertical-lr;
-}
+    .main {
+        text-align: center;
+        padding: var(--space-md);
+        background: var(--body-bgColor);
+        box-shadow: var(--shadow-xl)
+    }
 
-.worldmap { 
-    position: relative;
-    z-index: 1;
-    grid-area: 2 / 2 / 5 / 13; 
-}
+    .grid__button { 
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        position: relative;
+        z-index: 2;
+        grid-area: 1 / 11 / 6 / 13;
+    }
+
+    .grid__date { 
+        grid-area: 2 / 1 / 5 / 2; 
+        padding-left: var(--space-md)
+    }
+
+    .date {
+        text-transform: uppercase;
+        writing-mode: vertical-lr;
+        transform: rotate(180deg)
+    }
+
+    .grid__map { 
+        position: relative;
+        z-index: 1;
+        grid-area: 2 / 2 / 5 / 13; 
+    }
 </style>
