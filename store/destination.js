e8e659f34dfc00370670dcbbe7b30ab287cbb70f
Christian <chrs.albert@gmail.com>
2020-09-21 17:22:30 +0200

    updates destination store

--- store/destination.js
+++ store/destination.js
@@ -1,9 +1,13 @@
 export const state = () => ({
     currentDestination: null,
+    currentDestinationIndex: null
   })
 
 export const mutations = {
     SET_CURRENT_DESTINATION(state, val) {
         state.currentDestination = val
+    },
+    SET_CURRENT_DESTINATION_INDEX(state, val) {
+        state.currentDestinationIndex = val
     }
 }
