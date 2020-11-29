8592d2dc48abc3c3ec9b90c5d685772110373094
Christian <chrs.albert@gmail.com>
2020-11-29 22:50:05 +0100

    removes axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,3 @@
-import axios from 'axios'
 import destinations from './static/data/destinations.json'
 
 const dynamicRoutes = [];
@@ -9,45 +8,38 @@ destinations.forEach(el => {
   //   dynamicRoutes.push(`${el.id}/${i}`)
   // }
 });
-
 export default {
-  mode: 'universal',
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Merriweather:wght@700&family=Montserrat:wght@400;600&display=swap'},
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  components: true,
-  generate: {
-    routes: dynamicRoutes
-  },
-  env: {
-    baseUrl: process.env.BASE_URL || 'http://localhost:3000',
-    // imageUrl: process.env.IMAGE_URL || 'https://cdn.statically.io/img/world-rosy.now.sh/images/'
-    imageUrl: process.env.IMAGE_URL || 'http://localhost:3000/images'
-  },
-  loading: { color: '#fff' },
-  css: [
-    '~/assets/css/reset.css',
-    '~/assets/css/main.css'
-  ],
-  plugins: [
-  ],
-  modules: [
-    '@nuxtjs/axios',
-    "@nuxtjs/svg"
-  ],
-  axios: {
-  },
-  build: {
-    extend (config, ctx) {
-    }
-  }
+	mode: 'universal',
+	layoutTransition: {
+		name: 'layout',
+		mode: 'out-in'
+	},
+	head: {
+		title: process.env.npm_package_name || '',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+		],
+		link: [
+			{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Merriweather:wght@700&family=Montserrat:wght@400;600&display=swap'},
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+		]
+	},
+	components: true,
+	generate: {
+		routes: dynamicRoutes
+	},
+	env: {
+		baseUrl: process.env.BASE_URL || 'http://localhost:3000',
+		imageUrl: process.env.IMAGE_URL || 'http://localhost:3000/images'
+	},
+	loading: { color: '#fff' },
+	css: [
+		'~/assets/css/reset.css',
+		'~/assets/css/main.css'
+	],
+	modules: [
+		"@nuxtjs/svg"
+	]
 }
